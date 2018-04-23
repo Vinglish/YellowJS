2ed32221dcac8405f3b82d0616fac121d6cebcde
Vinglish <khan.ildar94@gmail.com>
2018-04-23 14:38:33 +0300

    Rere

---
+++ script.js
@@ -0,0 +1,21 @@
+var arr = [
+    [],
+    [],
+    [],
+    []
+];
+var pict = document.getElementById('pictures');
+function mass(obj) {
+    for(var i = 0; i<obj.length; i++){
+    	for(var k = 0; k<obj.length; k++){
+    		if(obj[3][2]){
+    			obj[3][2] =  innerHTML = "<img width='100px' src='img/green.jpg'>";
+    		}
+    	obj[i][k] = innerHTML = "<img width='100px' src='img/yellow.png'>"
+    	}
+    }
+    pict.innerHTML = obj;
+ }
+mass(arr);
+
+	
